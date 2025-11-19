f24e1884ec44375dc5af5d9d1a2eceb0aa9e3ad6
Tim Deutschman <timdeu05@gmail.com>
2025-11-18 17:47:55 -0800

    Created Player

--- Assets/Scripts/PlayerInputManager.cs
+++ Assets/Scripts/PlayerInputManager.cs
@@ -2,5 +2,9 @@ using UnityEngine;
 
 public class PlayerInputManager : MonoBehaviour
 {
-    
+    private PlayerMovement playerMovement;
+    private void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
 }
