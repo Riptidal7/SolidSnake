8c3da16a060b7ec743b7a48af3ea9b7ca11d4e2a
Tim Deutschman <timdeu05@gmail.com>
2025-11-18 18:52:14 -0800

    Added player movement and world boundaries

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerMovement : MonoBehaviour
 {
+    public SpriteRenderer SnakeSpriteRenderer;
     
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb.linearVelocity = moveInput * GameParameters.SnakeMoveSpeed;
+        KeepOnScreen();
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+    
+    private void KeepOnScreen()
+    {
+        SnakeSpriteRenderer.transform.position =
+            SpriteTools.ConstrainToScreen(SnakeSpriteRenderer);
+    }
 }
