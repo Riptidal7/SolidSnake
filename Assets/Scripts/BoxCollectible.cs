79a3a9ee0d41c4b38ca4eabea129b4a0d31a74d7
Tim Deutschman <timdeu05@gmail.com>
2025-11-20 15:30:35 -0800

    Collecting a box now attaches tail box
    The tail box does not follow snake in the correct way but first I want to make sure I can spawn boxes

--- Assets/Scripts/BoxCollectible.cs
+++ Assets/Scripts/BoxCollectible.cs
@@ -13,9 +13,37 @@ public class BoxCollectible : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        GameObject instantiatedTailBox = Instantiate(TailBox, SnakeHead.position, Quaternion.identity);
-        instantiatedTailBox.transform.parent = transform;
+        AddBoxToSnake(collision);
         Destroy(gameObject);
     }
+
+    private void AddBoxToSnake(Collision2D collision)
+    {
+        BoxTailManager tail = SnakeHead.GetComponent<BoxTailManager>();
+
+        if (tail == null)
+        {
+            Debug.LogError("No BoxTailManager found in SnakeHead parent!");
+            return;
+        }
+
+        Vector3 spawnPosition;
+
+        if (tail.TailBoxes.Count == 0)
+        {
+            spawnPosition = SnakeHead.position - SnakeHead.right * GameParameters.TailSegmentSeparation;
+        }
+        else
+        {
+            Transform lastSegment = tail.TailBoxes[tail.TailBoxes.Count - 1];
+            spawnPosition = lastSegment.position;
+        }
+        
+        GameObject newTail = Instantiate(TailBox, spawnPosition, Quaternion.identity);
+        newTail.transform.SetParent(SnakeHead);
+
+        tail.TailBoxes.Add(newTail.transform);
+    }
+
     
 }
--- Assets/Scripts/GameParameters.cs
+++ Assets/Scripts/GameParameters.cs
@@ -3,4 +3,5 @@ using UnityEngine;
 public static class GameParameters
 {
     public static float SnakeMoveSpeed = 5f;
+    public static float TailSegmentSeparation = 1.6f;
 }
